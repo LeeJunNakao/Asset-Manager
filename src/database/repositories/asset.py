066e62f934d1f04b803784c964c0f3eea6d2c9d9
Lee Jun <leejun.nakao@gmail.com>
2021-12-05 16:10:50 +0000

    refactor: use auxiliar functions in Asset Repository

--- src/database/repositories/asset.py
+++ src/database/repositories/asset.py
@@ -3,71 +3,24 @@ from typing import List
 from src.domain.entities.asset.dto import AssetCreateDto, AssetDto, AssetUpdateDto
 from src.database.model.asset import Asset as Model
 from src.database.config import Session
-from src.domain.exceptions.exceptions import FailedToCreate, FailedToFind, FailedToUpdate, FailedToDelete
-from src.database.exceptions.exceptions import UniqueViolationException, InexistentItem
+from src.database.repositories.utils.fns import persist, find_by_user, update, delete
 
 
 class AssetRepo:
     def __init__(self, session: Session):
         self._session = session
         self._entity = "asset"
+        self.model = Model
+        self.base_model = AssetDto
 
     def persist(self, dto: AssetCreateDto) -> AssetDto:
-        item = self._session.query(Model).filter(
-            Model.code == dto.code).filter(Model.user_id == dto.user_id).all()
-        if len(item):
-            raise UniqueViolationException(self._entity)
-        try:
-            data = Model(**dto.dict())
-            self._session.add(data)
-            self._session.commit()
-            self._session.refresh(data)
-            return AssetDto.from_orm(data)
-        except Exception:
-            self._session.rollback()
-            raise FailedToCreate(self._entity)
-        finally:
-            self._session.close()
+        return persist(self, dto)
 
     def find_by_user(self, user_id: int) -> List[AssetDto]:
-        try:
-            items = self._session.query(Model).filter(
-                Model.user_id == user_id).all()
-            return [AssetDto.from_orm(item) for item in items]
-        except Exception:
-            raise FailedToFind(self._entity)
-        finally:
-            self._session.close()
+        return find_by_user(self, user_id)
 
     def update(self, dto: AssetUpdateDto) -> AssetDto:
-        asset = self._session.query(Model).filter(
-            Model.id == dto.id).first()
-        if not asset:
-            raise InexistentItem(self._entity)
-        try:
-            for key, value in dto.dict(exclude_none=True).items():
-                setattr(asset, key, value)
-            self._session.commit()
-            return AssetDto.from_orm(asset)
-        except Exception:
-            self._session.rollback()
-            raise FailedToUpdate(self._entity)
-        finally:
-            self._session.close()
+        return update(self, dto)
 
     def delete(self, _id: int, user_id: int) -> None:
-        asset = self._session.query(Model).filter(
-            Model.id == _id
-        ).filter(
-            Model.user_id == user_id
-        ).first()
-        if not asset:
-            raise InexistentItem(self._entity)
-        try:
-            self._session.delete(asset)
-            self._session.commit()
-        except Exception:
-            self._session.rollback()
-            raise FailedToDelete(self._entity)
-        finally:
-            self._session.close()
+        return delete(self, _id, user_id)
